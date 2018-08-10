e37c2a9812be1b5cb669ef5bf7ba969d5f066606
Ashish1341 <ashish.1341@gmail.com>
2018-08-10 09:46:59 +0530

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Spiderman', clicks: 0 },
+  { id: 12, name: 'Iron Man', clicks: 0 },
+  { id: 13, name: 'Captain America', clicks: 0 },
+  { id: 14, name: 'Hulk', clicks: 0 },
+  { id: 15, name: 'Thor', clicks: 0 },
+  { id: 16, name: 'Superman', clicks: 0 },
+  { id: 17, name: 'Batman', clicks: 0 },
+  { id: 18, name: 'Dr. Strange', clicks: 0 },
+  { id: 19, name: 'Antman', clicks: 0 },
+  { id: 20, name: 'Shaktiman', clicks: 0 }
+];
